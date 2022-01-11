6fa535c14ac8fc207147d7056182d7c161b8821b
Renan Ferreira <renan.ferreira@iohk.io>
2022-01-11 17:08:44 -0300

    [DDW-860] Fix update button styling

--- source/renderer/app/components/wallet/WalletSendForm.js
+++ source/renderer/app/components/wallet/WalletSendForm.js
@@ -850,7 +850,6 @@ export default class WalletSendForm extends Component<Props, State> {
 
     const addAssetButtonClasses = classNames([
       styles.addAssetButton,
-      !this.hasAvailableAssets ? styles.disabled : null,
       'primary',
     ]);
 
